fcb584f8ca8fee935eb87ef9e6190c927b702729
EmanRashid <emansiddiqui2781@gmail.com>
2025-10-15 23:09:00 +0500

    Add files via upload
    This program uses two integer variables a and b.
    It first prompts the user to enter the values of a and b.
    Then, it calculates and displays:
    The sum (a + b)
    The difference (a - b)
    The product (a * b)
    The quotient (a / b)

---
+++ arithmetic_operations.c.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b;
+	
+	printf("Enter a");
+	scanf("%d",&a);
+	
+	printf("Enter b");
+	scanf("%d",&b);
+	
+	printf("the sum of a and b is %d \n",a+b);
+	printf("the difference of a and b is %d \n",a-b);
+	printf("the product of a and b is %d \n",a*b);
+	printf("the quotient of a and b is %d \n",a/b);
+	
+	return 0;
+}
