5cc540938ad0612db5f0ebe1159114846706a96b
EmanRashid <emansiddiqui2781@gmail.com>
2025-10-23 23:56:04 +0500

    Add files via upload
    This program takes a single character input from the user and determines whether it is a vowel or a consonant.

---
+++ vowel_identifier.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter any alphabet: \n");
+    scanf(" %c",&ch);
+    
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+    printf("It's a vowel");
+    else
+    printf("It's a consonant");
+    
+
+    return 0;
+}
